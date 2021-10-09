3087da62f08418a86c5daed892f13858e4031064
Pedro Wanderley <peu-zico@hotmail.com>
2021-10-08 22:57:21 -0300

    start implementing csv file upload with stream

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,19 @@
+import csvParse from "csv-parse";
+import fs from "fs";
+
+class ImportCategoryUseCase {
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    // pega o pedaço lido do file e passa para o parseFile
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
+  }
+}
+
+export { ImportCategoryUseCase };
